9b9aa08110f81f6b0d3194c2cd936830d709b5e7
CristinaAlina <sirbu.cristina.alina94@gmail.com>
2023-12-09 22:03:21 +0200

    Created Paddle class inheriting from Turtle class for creating the paddle particularities and movement features

---
+++ paddle.py
@@ -0,0 +1,45 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+
+
+class Paddle(Turtle):
+    def __init__(self, x_coordinate, y_coordinate):
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+        self.paddle_body = []
+        self.create_paddle(self.x_coordinate)
+        self.last_element_idx = len(self.paddle_body)-1
+        self.first_element_idx = 0
+        self.head_paddle = self.paddle_body[self.first_element_idx]
+
+    def create_paddle(self, x_cor):
+        """Create a paddle with 4 squares"""
+        for index_square in range(4):
+            new_square = Turtle("square")
+            new_square.penup()
+            new_square.color("white")
+            new_square.pensize(20)
+            new_square.speed("fastest")
+            self.paddle_body.append(new_square)
+            new_square.goto(x_cor, index_square * 20)
+
+    def move(self, direction):
+        for square_num in range(self.last_element_idx, self.first_element_idx-1, -1):
+            if direction == "up":
+                new_y = self.paddle_body[square_num].ycor() + MOVE_DISTANCE
+            elif direction == "down":
+                new_y = self.paddle_body[square_num].ycor() - MOVE_DISTANCE
+            self.paddle_body[square_num].goto(self.x_coordinate, new_y)
+
+    def up(self):
+        self.head_paddle = self.paddle_body[self.last_element_idx]
+        if self.head_paddle.ycor() < self.y_coordinate:
+            self.move("up")
+
+    def down(self):
+        self.head_paddle = self.paddle_body[self.first_element_idx]
+        if self.head_paddle.ycor() > -self.y_coordinate:
+            self.move("down")
