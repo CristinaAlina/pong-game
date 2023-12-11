310f34aeb6df68ef6ebc500e9d256528f8cd65ed
CristinaAlina <sirbu.cristina.alina94@gmail.com>
2023-12-11 19:44:59 +0200

    Added case for detection collision with paddle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-# TODO: Detect collision with paddle
+
 # TODO: Create the scoreboard
 # TODO: Increase the score to one player if another one loses the ball
 # TODO: Win the first player that reached score 10
@@ -20,12 +20,14 @@ screen.title("Ping Pong Game")
 screen.tracer(0)
 
 board_game = BoardGame(height=HEIGHT)
-half_screen_height = board_game.half_screen_height - 10
-half_screen_width = WIDTH / 2 - 15
+half_height = board_game.half_screen_height - 10
+half_width = WIDTH / 2 - 15
 
-player1_paddle = Paddle(-half_screen_width + 10, half_screen_height)
-player2_paddle = Paddle(half_screen_width - 15, half_screen_height)
 
+player1_paddle = Paddle(-half_width + 10, half_height)
+player2_paddle = Paddle(half_width - 15, half_height)
+paddle1_body = player1_paddle.paddle_body
+paddle2_body = player2_paddle.paddle_body
 
 ball = Ball()
 
@@ -38,16 +40,22 @@ screen.onkeypress(fun=player2_paddle.down, key="Down")
 game_over = False
 while not game_over:
     screen.update()
-
     ball.move()
 
-    # TODO: Detect collision with wall and bounce
-    if ball.ycor() > half_screen_height or ball.ycor() < -half_screen_height:
+    bounce_angle = ball.heading()  # stay the course
+    # Detect collision with wall and bounce
+    if ball.ycor() > half_height or ball.ycor() < -half_height:
         bounce_angle = 360 - ball.heading()  # bounce off bottom/top walls
-    elif ball.xcor() > half_screen_width or ball.xcor() < -half_screen_width:
-        bounce_angle = 180 - ball.heading()  # bounce off right/left walls
-    else:
-        bounce_angle = ball.heading()  # stay the course
-    ball.setheading(bounce_angle)
+    elif ball.xcor() > half_width or ball.xcor() < -half_width:
+        ball.set_random_location()  # reset the ball position
+
+    # Detect collision with paddle
+    # Bounce off left/right paddle
+    if ball.is_collision_paddle(paddle1_body) or ball.is_collision_paddle(paddle2_body):
+        bounce_angle = 180 - ball.heading()
+
+    if ball.heading() != bounce_angle:
+        ball.setheading(bounce_angle)
+
 
 screen.exitonclick()
