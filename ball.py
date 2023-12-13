cf43828073d77f5de741835073e9462d5bf90e84
CristinaAlina <sirbu.cristina.alina94@gmail.com>
2023-12-14 00:07:49 +0200

    Added verification for out of area case

--- ball.py
+++ ball.py
@@ -6,18 +6,29 @@ class Ball(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("circle")
-        self.color("blue")
-        self.penup()
+        self.color("indigo")
         self.speed("fastest")
+        self.penup()
+        self.shapesize(stretch_wid=1, stretch_len=1, outline=1)
         self.set_random_location()
 
     def set_random_location(self):
-        angles_list = [0, 25, 35, 45, 300]
-        x = - random.randint(300, 400)
+        angles_list = [25, 35, 45, 70, 160, 240, 300]
+        x = 0
         y = random.randint(-270, 270)
+        self.goto(x, y)
         angle = random.choice(angles_list)
         self.setheading(angle)
-        self.goto(x, y)
+
+    def is_collision_paddle(self, paddle_body):
+        """Returns True if it detects collision with paddle, or False otherwise"""
+        for paddle_square in paddle_body:
+            if self.distance(paddle_square) < 22:
+                return True
+        return False
 
     def move(self):
-        self.forward(30)
+        self.forward(15)
+
+    def is_out_of_area(self, width, height):
+        return self.xcor() > width or self.xcor() < -width or self.ycor() > height or self.ycor() < -height
